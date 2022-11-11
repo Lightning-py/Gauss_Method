3401d7f11387d8a49a3df1141b67d94607ce392c
Lightning <sergejananev78@gmail.com>
2022-11-11 20:21:24 +0300

    Добавил проверку матрицы на "решаемость" в целом. Конкретно сделал алгоритм, который ищет строку с ненулевым элементов вида a[k][k] если таковой оказался нулевым в исходной строке.
    Тем самым решил баг с "вылетами в бесконечность" некоторых корней для
    нерешаемых/неправильно расположенных матриц.

--- main.cpp
+++ main.cpp
@@ -16,17 +16,15 @@
 
 int main()
 {
-    setlocale(0, "");
-
     // исходная матрица, ввод сделаю потом
 
     // пусть вектор matrix будет исходной дополненной матрицей 
 
     std::vector < std::vector <float> > matrix = 
     {
-        {1, 2, 4, 5},
-        {2, 3, 5, 7},
-        {5, 6, 2, 3}
+        {5, 3, 1, 1},
+        {4, 0, 2, 10},
+        {0, 1, 0, 2}
     };
 
     const int matrix_size = matrix.size(); // создадим переменную, которая будет хранить размер матрицы (матрица квадратная, поэтому храним только одно число)
@@ -52,9 +50,30 @@ int main()
 
     // предположим, что a11 != 0. разделим на это число коэфициенты первого уравнения
 
-    for (int element_index_now = 0; element_index_now < matrix_size; element_index_now++)
+    for (unsigned int element_index_now = 0; element_index_now < matrix_size; element_index_now++)
     {
         float first_element = matrix[element_index_now][element_index_now];
+
+        if (first_element == 0) // если нужный элемент индексом akk равен нулю, то найдем другую строку на место этой, в которой на этом месте нет нулей
+        {
+
+            int index_needed = -1;
+
+            for (int i = element_index_now; i < matrix_size; i++)
+            {
+                if (matrix[i][element_index_now] != 0)
+                {
+                    index_needed = i;
+                    break;
+                }
+            }
+
+            if (index_needed == -1)
+            {
+                std::cout << "matrix cannot be solved" << std::endl;
+                exit(0);
+            }
+        }
 
         for (unsigned int element_index = 0; element_index < matrix_size + 1; element_index++)
         {
@@ -75,7 +94,7 @@ int main()
         }
     }
 
-    std::cout << "матрица, приведенная к ступенчатому виду" << std::endl;
+    std::cout << "matrix after the Gauss algo first part" << std::endl;
 
     for (auto vect: matrix)
     {
@@ -103,7 +122,7 @@ int main()
 
     }
 
-    std::cout << std::endl << "ответы" << std::endl;
+    std::cout << std::endl << "answers" << std::endl;
 
     for (int i = 0; i < matrix_size; i++)
     {
