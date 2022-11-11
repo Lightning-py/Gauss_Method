8b47d4f97d9676066e9a8aea5dff5e3e9f811a07
Lightning <sergejananev78@gmail.com>
2022-11-11 10:59:01 +0300

    Полностью закончил сам алгоритм.
    Возможно, потом сделаю ввод/вывод матрицы и проверку на наличие корней
    через детерминант.

--- functions.hpp
+++ functions.hpp
@@ -1,13 +1,45 @@
-#pragma once
+#pragma once // не знаю вообще зачем добавил, один раз все подключаю в main.cpp, но пусть будет на всякий
 
 
 #include <vector>
 
-// struct Matrix
-// {
-    
-// };
 
+
+
+/*
+Альфа преобразования для всей матрицы.
+Преобразования, заключающиеся в домножении всей матрицы на скаляр.
+*/
+void matrix_multiply(std::vector < std::vector <float> > &matrix, float multiply_number)
+{
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix[i].size(); j++)
+        {
+            matrix[i][j] = matrix[i][j] * multiply_number;
+        }
+    }
+}
+
+
+/*
+Гамма преобразования.
+Преобразования, заключающиеся в добавлении к строке чисел, кратных другой строке.
+*/
+void line_plus_line(std::vector < std::vector <float> > &matrix, unsigned int line_1, unsigned int line_2, float number)
+{
+    for (int i = 0; i < matrix[0].size(); i++)
+    {
+        matrix[line_1][i] += matrix[line_2][i] * number;
+    }
+}
+
+
+
+/*
+Функция, позволяющая определить ряд с наименьшим первым числом.
+Помогает при сортировке матрицы путем замены строк.
+*/
 unsigned int find_min_firstline_element(std::vector < std::vector <float> > matrix, unsigned int line_start)
 {
     float min_now = matrix[line_start][0];
@@ -25,6 +57,11 @@ unsigned int find_min_firstline_element(std::vector < std::vector <float> > matr
     return index_min;
 }
 
+
+/*
+Косметические преобразования матрицы.
+Преобразования заключаются в изменении порядка строк.
+*/
 void line_swap(std::vector <std::vector <float> > &matrix, unsigned int line_1, unsigned int line_2)
 {
     std::vector <float> swap_line_vector = matrix[line_2];
@@ -33,24 +70,21 @@ void line_swap(std::vector <std::vector <float> > &matrix, unsigned int line_1, 
     matrix[line_1] = swap_line_vector;
 }
 
+
+/*
+Альфа преобразования.
+Преобразования, заключающиеся в домножении конкретной строки матрицы на скаляр.
+*/
 void line_multiply(std::vector <std::vector <float> > &matrix, unsigned int line_number, float multiply_number)
 {
     for (int i = 0; i < matrix[line_number].size(); i++) { matrix[line_number][i] = matrix[line_number][i] * multiply_number; }
 }
 
 
-void matrix_multiply(std::vector < std::vector <float> > &matrix, float multiply_number)
-{
-    for (int i = 0; i < matrix.size(); i++)
-    {
-        for (int j = 0; j < matrix[i].size(); j++)
-        {
-            matrix[i][j] = matrix[i][j] * multiply_number;
-        }
-    }
-}
-
-
+/*
+Косметические преобразования.
+Преобразования, заключающиеся в сортировке строк матрицы для того чтобы глазкам было не больно смотреть))))
+*/
 void matrix_lines_sort(std::vector < std::vector <float> > &matrix)
 {
     for (int i = 0; i < matrix.size(); i++)
@@ -64,4 +98,3 @@ void matrix_lines_sort(std::vector < std::vector <float> > &matrix)
 
     }
 }
-
--- main.cpp
+++ main.cpp
@@ -1,16 +1,114 @@
+/*
+Реализация метода Гаусса взята из учебника Ф.Г. Авхадиева "Основы численных методов"
+
+ссылка на учебник: https://kpfu.ru//staff_files/F240899232/OsnChM_Avkhadiev_FG.pdf
+
+*/
+
+
 #include <iostream>
 #include <vector>
+#include <map>
 
 
-
-#include "functions.hpp"
+#include "functions.hpp" // импорт функций, связанных с матрицей и преобразованиями
 
 
 int main()
 {
+    setlocale(0, "");
+
+    // исходная матрица, ввод сделаю потом
+
+    // пусть вектор matrix будет исходной дополненной матрицей 
+
+    std::vector < std::vector <float> > matrix = 
+    {
+        {1, 2, 4, 5},
+        {2, 3, 5, 7},
+        {5, 6, 2, 3}
+    };
+
+    const int matrix_size = matrix.size(); // создадим переменную, которая будет хранить размер матрицы (матрица квадратная, поэтому храним только одно число)
+    // тут появился небольшой баг, так как у меня ширина матрицы на 1 больше чем длина ибо матрица изначально расширенная, но вроде все решилось быстро
 
 
+    // контейнер с ответами
+
+    std::map <unsigned int, float> answers;
+
+
+
+    /*
+    
+    реализация метода Гаусса для заданной матрицы (способом ввода данных займусь потом)
+    метод работает для квадратных матриц, не имеющих нулевых коэфициентов в первом столбце
+
+    начну с прямого хода
+
+    */
+    
+
+
+    // предположим, что a11 != 0. разделим на это число коэфициенты первого уравнения
+
+    for (int element_index_now = 0; element_index_now < matrix_size; element_index_now++)
+    {
+        float first_element = matrix[element_index_now][element_index_now];
+
+        for (unsigned int element_index = 0; element_index < matrix_size + 1; element_index++)
+        {
+            matrix[element_index_now][element_index] /= first_element;
+        }
+
+        // умножим новое первое уравнения на число ak1 и вычтем из k-го уравнения
+
+        for (int k = element_index_now + 1; k < matrix_size; k++)
+        {
+
+            float a_K_1 = matrix[k][element_index_now]; // первый элемент k-го уравнения
+
+            for (int i = 0; i < matrix_size  + 1; i++)
+            {
+                matrix[k][i] -= matrix[element_index_now][i] * a_K_1;
+            }
+        }
+    }
+
+    std::cout << "матрица, приведенная к ступенчатому виду" << std::endl;
+
+    for (auto vect: matrix)
+    {
+        for (auto element: vect)
+        {
+            std::cout << element << ' ';
+        }
+        std::cout << std::endl;
+    }
+
+
+    answers[matrix_size - 1] = matrix[matrix_size - 1][matrix_size];
+
+
+    // теперь обратный ход (как прямой - только обратный)
+
+    for (int i = matrix_size - 1; i >= 0; i -= 1)
+    {
+        float answer = matrix[i][matrix_size];
+        for (int element_index = matrix_size - 1; element_index > i; element_index -= 1)
+        {
+            answer -= answers[element_index] * matrix[i][element_index];
+        }
+        answers[i] = answer;
+
+    }
+
+    std::cout << std::endl << "ответы" << std::endl;
+
+    for (int i = 0; i < matrix_size; i++)
+    {
+        std::cout << 'x' << i + 1 << " = " << answers[i] << std::endl;
+    }
 
     return 0;
 }
-
