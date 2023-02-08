2208d2bd13ea4620bcce0538f0867221c8eb57fc
Lightning <sergejananev78@gmail.com>
2023-02-08 13:25:47 +0300

    Made some test to find errors in algorythm

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 
 */
 
+bool DEBUG = false;
+bool FILE_OUTPUT = true;
+
 
 #include <iostream>
 #include <vector>
@@ -14,30 +17,41 @@
 
 #include "functions.hpp" // импорт функций, связанных с матрицей и преобразованиями
 
-
 int main()
-{
+{    
+
+
     Matrix my_matrix;
-    my_matrix.matrix_body = {
-        {1, 2, 3},
-        {4, 9, 13},
-        {9, 19, 29}
-    };
-    my_matrix.addition = {10, 41, 92};
-    my_matrix.rows = 3;
+    
+    matrix_in(my_matrix, "INPUT.TXT");
 
-
-    std::cout << "Input:" << std::endl;
-    matrix_out(my_matrix);
+    if (DEBUG)
+    {
+        std::cout << "Input:" << std::endl;
+        matrix_out(my_matrix);
+    }
 
     gauss_algo_first_part(my_matrix);
 
-
-    std::cout << std::endl << "Matrix after gauss algo first part:" << std::endl;
-    matrix_out(my_matrix);
+    if (DEBUG)
+    {
+        std::cout << std::endl << "Matrix after gauss algo first part:" << std::endl;
+        matrix_out(my_matrix);
+    }
+    
 
     gauss_algo_second_part(my_matrix);
-    matrix_answers_out(my_matrix);
+    if (FILE_OUTPUT)
+    {
+        matrix_out_file(my_matrix, "OUTPUT.TXT");
+    }
+    else
+    {
+        std::cout << std::endl;
+        matrix_out(my_matrix);
+        matrix_answers_out(my_matrix);
+    }
+
 
     return 0;
 }
